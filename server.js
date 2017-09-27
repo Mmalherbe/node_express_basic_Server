e1bab1136b0f464e433bee27e7685deb069939a6
Mpr Malherbe <max@mprmalherbe.com>
2017-09-27 09:32:44 +0200

    (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ socketio basic

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 // Allow Cross-Origin requests, just leave this as is
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -7,27 +7,10 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.use(express.static(__dirname +'/client'));
-//Store all HTML files in view folder.
-
-app.get('/',function(req,res){
-    res.sendFile('/index.html');
-    //It will find and locate index.html from View or Scripts
+let server = require('http').createServer();
+let io = require('socket.io')(server);
+io.on('connection', function(client){
+    client.on('event', function(data){});
+    client.on('disconnect', function(){});
 });
-
-app.get('/', function (req, res) {
-    // respond to a GET request without anything after the url
-
-    jsonfile.readFile(file, function(err, obj) {
-        console.dir(obj);
-        res.send(obj);
-    });
-
-
-
-});
-
-
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
+server.listen(3000);
